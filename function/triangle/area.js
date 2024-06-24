02a53a0d32d3cb9f4334f2419e451fc705f09bee
programmer-arnav <arnavgarg195@gmail.com>
2024-06-24 17:18:20 +0530

    I have created area.js

---
+++ function/triangle/area.js
@@ -0,0 +1,13 @@
+/*
+
+
+*/
+function area_of_triangle(h,b){
+    const a =(1/2)* base*height
+
+    return a 
+}
+let height = 4;
+let base = 5;
+let result =area_of_triangle(height,base) ;
+console.log(area_of_triangle(4,5),"cm^2")
