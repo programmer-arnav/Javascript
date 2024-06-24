6673f6622d130905aa21f14644d54549a4a8c3ff
programmer-arnav <arnavgarg195@gmail.com>
2024-06-24 17:25:41 +0530

    I added perimeter.js

---
+++ function/triangle/perimeter.js
@@ -0,0 +1,16 @@
+/*
+
+Perimeter = base + height + hypotenuse
+ b = base of triangle
+ h = height of triangle
+ hy =hypotenuse of triangle 
+ unit =cm
+*/
+function Perimeter_of_triangle(a,b,c){
+let Perimeter = a+b+c
+console.log(Perimeter)
+}
+let a = 5
+let b = 5
+let c = 5
+Perimeter_of_triangle(a,b,c)
