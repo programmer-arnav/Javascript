06cd8e4ff921d1243c496458b6bac12d295d48d9
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 15:49:32 +0530

    I added this file

---
+++ function/paralellogram/area.js
@@ -0,0 +1,12 @@
+/*formula = b*h
+ b = base
+ h = height
+ */
+function area_of_parallelogram(l,b){
+    const x = base*height
+    return x 
+}
+let base = 6
+let height = 9
+let result = area_of_parallelogram(base,height)
+console.log(result)
