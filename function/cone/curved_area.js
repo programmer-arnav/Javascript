f8aed00436ea1ad3b68944ea49780af45262119a
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 15:57:53 +0530

    I added this file

---
+++ function/cone/curved_area.js
@@ -0,0 +1,15 @@
+/*formula=Curved Surface Area, Ac = πrl
+PI= 3.14;
+r= Radius
+l= slant height 
+ */
+function Curved_surface_area(π,r,l){
+    const a= π*r*l
+    Pi = 3.14
+    return a
+}
+let Radius = 6
+let length = 4
+let π = 3.14
+let result = Curved_surface_area(Radius,length,π)
+console.log(Curved_surface_area(6,4,3.14),"cm^2")
