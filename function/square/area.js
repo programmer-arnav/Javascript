f1d974275eee2c357d52bbc15151745594fc420e
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 22:11:36 +0530

    I added this file

---
+++ function/square/area.js
@@ -0,0 +1,11 @@
+/*  area of square 
+formula = a**2
+a = side 
+ */
+function area_of_square(a){
+    const x = a**2
+    return x
+}
+let a = 12
+let result = area_of_square(a)
+console.log(result)
