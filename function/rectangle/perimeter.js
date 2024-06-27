0935d01d5d348a110a46fd4d54eb7414282e5cc7
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 15:48:05 +0530

    I added this file

---
+++ function/rectangle/perimeter.js
@@ -0,0 +1,13 @@
+/*
+formula =2(l+w) 
+l = length
+w = width
+*/
+function perimeter_of_rectangle(l,w){
+    const a =2*(l+w)
+    return a
+}
+let length = 5
+let width = 8
+let result = perimeter_of_rectangle(length,width)
+console.log(result,"cm^2")
