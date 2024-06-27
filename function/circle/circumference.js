93285a3e39e7572af7304c93e2ca69e9eaec2285
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 16:01:45 +0530

    I added this file

---
+++ function/circle/circumference.js
@@ -0,0 +1,12 @@
+/* formula = 2πr
+r = radius
+π = 3.14
+ */
+function circumference_of_circle(π,r){
+    const a = 2*π *r
+    return a
+}
+let π = 3.14
+let r = 8
+let result = circumference_of_circle(π,r)
+console.log(result)
