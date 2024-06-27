155fdd18d126c77876b56e56cae11cc39e8ef3c2
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 15:46:05 +0530

    I added this file

---
+++ function/trapezium/area.js
@@ -0,0 +1,11 @@
+/*formula = ½(a + b)h
+ */
+function area_of_trapezium(l,b,h){
+    const A = 1/2*( c + b)*h
+    return A
+}
+ let height = 8
+ let c = 3
+ let b = 5
+ let result = area_of_trapezium(height, c, b)
+ console.log(result)
