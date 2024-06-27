65c20bdeb58b91fcbf8e92cae54044f2e1c8c887
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 15:46:54 +0530

    I added this file

---
+++ function/rectangle/area.js
@@ -0,0 +1,12 @@
+/* formula = l*w
+   l = length
+   w = width
+ */
+ function area_of_rectangle(l,w){
+    const a = l*w    
+     return a
+ }
+ let length = 5
+ let width = 7
+ let result = area_of_rectangle(length,width)
+ console.log(area_of_rectangle(5,7),"cm^2")
