863d28ce7f814c30fea37f94faf830a83e683aab
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 16:48:06 +0530

    I added this file

---
+++ function/cuboid/volume.js
@@ -0,0 +1,15 @@
+/* volume of cuboid 
+ formula = lbh
+l= length
+b = breadth
+h = height
+ */
+function volume_of_cuboid(l,b,h){
+    const a = l*b*h
+    return a 
+}
+let l = 3
+let b = 4
+let h = 5
+let result = volume_of_cuboid(l,b,h)
+console.log(result)
