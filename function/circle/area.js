c12dece4dc36a9c971bb66d77bed9a998f9dad6b
programmer-arnav <arnavgarg195@gmail.com>
2024-06-27 16:00:10 +0530

    i added this file

---
+++ function/circle/area.js
@@ -0,0 +1,13 @@
+
+/* formula =  πr2
+   r  = radius 
+    π = 3.14
+ */
+function area_of_circle( π,r){
+    const a =  π*r**2
+    return a 
+}
+let r = 9
+let  π = 3.14
+let result = area_of_circle(r, π) 
+console.log(result)
